bdde3498ba91d177d359ddf6cbbffd1447f9934d
salmansaheer <salmanvk23yr@gmail.com>
2022-12-12 09:17:49 +0530

    add home page

--- HOSPITAIL UNION/application/main.py
+++ HOSPITAIL UNION/application/main.py
@@ -1,1 +1,11 @@
-print('hii')
+from flask import Flask,Blueprint,render_template
+from src.home.api.blueprints import home
+
+app = Flask(__name__, static_url_path="/")
+
+app.register_blueprint()
+
+app.register_blueprint(home)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
